8860eea765c65e221a16a0fc72e8624e12ef4b61
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-10-27 15:52:42 +0900

    accounts-login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 def signup(request):
     if request.method == 'POST':
@@ -13,3 +15,16 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)

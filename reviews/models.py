6588b2dc8e2f840660e0de787960b086d3f27ea5
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-10-28 10:48:51 +0900

    review-model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    cafe_name = models.CharField(max_length=80)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
+    grade_choices = (
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    )
+    grade = models.IntegerField(choices=grade_choices)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
